8cda00cf5597dce582ae55f31b7107029ec3aa94
Mohammad Termzee <m.termzee@basecom.de>
2023-05-10 10:14:51 +0200

    Wildcard Routes({slug}) and add symfony/string

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -24,4 +24,17 @@ class HomeController extends AbstractController
         //return $this->render('home.html.twig');
         return new Response('Hello World');
     }
+
+    //wildcard Routes
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = str_replace('-', ' ', $slug);
+        } else {
+            $title = 'Browse our collection of Genres';
+        }
+
+        return new Response('Genre: ' . $title);
+    }
 }
