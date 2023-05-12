d0c987278fc4d9184c9661caf1ef7e90e0ac47a9
Mohammad Termzee <m.termzee@basecom.de>
2023-05-12 10:11:00 +0200

    add debug Parameters

--- src/Service/MixRepositrory.php
+++ src/Service/MixRepositrory.php
@@ -11,17 +11,19 @@ class MixRepositrory
     // Dependency Injection
     private HttpClientInterface $httpClient;
     private CacheInterface $cache;
+    private bool $isDebug;
 
     public function __construct(HttpClientInterface $httpClient, CacheInterface $cache)
     {
         $this->httpClient = $httpClient;
         $this->cache = $cache;
+        $this->isDebug = false;
     }
 
     public function findAll(): array
     {
         return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
-            $cacheItem->expiresAfter(5);
+            $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
             $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
 
             return $response->toArray();
